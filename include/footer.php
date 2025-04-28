251f0dcaf3a882a9168a7d9472d9e2d03bec7f2c
Samir Gharti Magar <sghartim0@gmail.com>
2025-04-28 15:30:42 +1000

    photos and sql

---
+++ include/footer.php
@@ -0,0 +1,22 @@
+	<footer>
+		<p style ="text-align: center;">&copy; CoreFlex Team, MIT licensed Copyright, All rights reservered, 20xx, CoreFlex.PLT</p>
+
+			
+		<a href="#" class = "social-media-icon"><i class="fa-brands fa-facebook"></i></a>
+		<a href="#" class = "social-media-icon"><i class="fa-brands fa-x-twitter"></i></a>
+		<a href="#" class = "social-media-icon"><i class="fa-brands fa-instagram"></i></a>
+		<a href="#" class = "social-media-icon"><i class="fa-brands fa-google-plus"></i></a>
+		<a href="#" class = "social-media-icon"><i class="fa-brands fa-linkedin"></i></a>
+		<a href="#" class = "social-media-icon"><i class="fa-brands fa-youtube"></i></a>
+		<a href="#" class = "social-media-icon"><i class="fa-brands fa-pinterest"></i></a>
+		<a href="#" class = "social-media-icon"><i class="fa-brands fa-flickr"></i></a>
+
+	</footer>
+
+	<!-- Link to backend -->
+	<p style ="text-align: center;"><a href = "_be_interface.php" ><img src ="images/space.gif" width = "150" height = "60" border = 0 /></a></p>
+
+
+</body>
+</html>
+ 
